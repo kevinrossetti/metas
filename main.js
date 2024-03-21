abe92def6b9be602d7e86e0facd973451e50a69d
kevinrossetti <105368440+kevinrossetti@users.noreply.github.com>
2024-03-21 08:47:31 -0300

    SAVE

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-const contadores = document.querySelectorAll(".contador");
 
 
 for(let i=0; i <botoes.length; i++){
@@ -13,6 +12,7 @@ for(let i=0; i <botoes.length; i++){
         textos[i].classList.add("ativo");        
     }
 }
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-21T00:00:00");
 let tempoAtual = new Date();
-contadores[0].textContext = tempoObjetivo1;
+contadores[0].textContext = tempoObjetivo1 - tempoAtual;
